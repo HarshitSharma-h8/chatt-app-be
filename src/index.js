49f45fc07eb30f265af088f275c06d510bf2470b
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2025-01-31 15:40:44 +0530

    Add CORS and cookie-parser middleware; update user model and routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-
+import cors from "cors";
+import cookieParser from "cookie-parser";
 // Import
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import dbConnection from "./lib/dbconnection.js";
+
 
 
 
@@ -13,9 +16,15 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 // middileware 
-app.use(express.json());  
-
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cookieParser());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.get("/", (req, res) => {
   res.send("<h1> App is running...</h1>");
